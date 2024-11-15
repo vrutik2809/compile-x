1dd5f935629fb2b5e22c5a8a211dc2a35dd2fce5
Vrutik Prajapati <vrutikprajapati2809@gmail.com>
2024-11-15 20:13:13 +0530

    feat: add main file

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/vrutik2809/compile-x/core"
+	"github.com/vrutik2809/compile-x/api"
+)
+
+func main() {
+	server := &http.Server{
+		Addr: ":8080",
+	}
+	http.HandleFunc("/api/execute", api.HandleCodeExecution)
+	go func() {
+		log.Printf("Starting server on %s", server.Addr)
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+		log.Println("Stopped serving new connections.")
+	}()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownRelease()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
+	}
+	log.Println("Graceful shutdown complete.")
+
+	core.ContainerPoolWait()
+
+}
+
+
